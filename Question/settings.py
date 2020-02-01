a226b3d57f1c3e407131fae6c90e448855b4e8ab
MasterTos <MasterTos@yahoo.com>
2020-02-01 16:54:57 +0700

    Update settings.py

--- Question/settings.py
+++ Question/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': os.environ['POSTGRES_DB'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': postgres,
+        'HOST': question-db,
         'PORT': 3306
     }
 }
